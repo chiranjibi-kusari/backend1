589316a872fa558a4ea93c9f1a24bb99872ebc1b
chiranjibi-kusari <chiranjibikusari1@gmail.com>
2024-08-06 20:20:28 +0545

    utils solve

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // require('dotenv').config({path:'./env'});
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({path:"./env"})
 
-connectDB();
+connectDB()
+.then(()=>{
+      app.listen(process.env.PORT || 5000,()=>{
+            console.log("server is running");
+            
+      })
+})
+.catch((error)=>{
+      console.log(("mongoo db connect failed !!",error));
+      
+})
 
 
 /*
