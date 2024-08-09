fab40ec238b073676782557f986bead17d1af651
chiranjibi-kusari <chiranjibikusari1@gmail.com>
2024-08-09 06:58:44 +0545

    register and controll

--- src/controllers/user.controll.js
+++ src/controllers/user.controll.js
@@ -1,6 +1,53 @@
 import { asyncHandler } from "../utils/asynchandler.js";
+import { ApiError } from "../utils/apiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser=asyncHandler(async (req,res)=>{
-     res.status(200).json({mesage:"hello man"})
+      const {username,email,fullname,password}=req.body
+      console.log("email: ",email);
+
+      if ([fullname,username,email,password].some((field)=> field?.trim()==="")) {
+            throw new ApiError(400, "All fields are required")     
+      }
+  const existedUser=await User.findOne({
+            $or:[{username},{email}]
+      })
+      if(existedUser){
+            throw new ApiError(409," email or username is Already exists")
+      }
+//     const avatarLocalPath=req.files?.avatar[0]?.path;
+    const coverfImageLocalPath=req.files?.coverImage[0]?.path;
+//     if (!avatarLocalPath) {
+//       throw new ApiError(400,"Avatar file is require")
+//     }
+//    const avatar=await uploadOnCloudinary(avatarLocalPath)
+
+  const coverImage=await uploadOnCloudinary(coverfImageLocalPath)
+
+//   if (!avatar) {
+//       throw new ApiError(400,"Avatar file is require"); 
+//   }
+
+  const user=await User.create({
+      fullname,
+      // avatar:avatar.url,
+     
+      coverImage:coverImage?.url|| "",
+      email,
+      password,
+      username:username.toLowerCase()
+  })
+ const createUser=await User.findById(user._id).select(
+      "-password -refreshToken"
+ );
+ if (!createUser) {
+      throw new ApiError(500,"something went wrong while registering the user")
+ }
+
+ return res.status(201).json(
+      new ApiResponse(200,createUser,"user registered successfully")
+ )
 })
 export {registerUser}
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 import {v2 as cloudinary} from "cloudinary"
-import fs from "fs "
+import fs from "fs"
 
 cloudinary.config({
       cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
@@ -14,7 +14,8 @@ const uploadOnCloudinary=async (localFilePath)=>{
                const response=await cloudinary.uploader.upload(localFilePath,{
                         resource_type:"auto"
                   })
-                  console.log("file is uploaded on cloudinary",response.url);
+                  // console.log("file is uploaded on cloudinary",response.url);
+                  fs.unlinkSync(localFilePath)
                   return response
             }
       } catch (error) {
